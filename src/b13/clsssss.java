63443d04e38d626725ee7db992395c861cd97a45
S1DED <andreylyubimenko@mail.ru>
2020-01-22 20:18:06 +0300

    lesson9_java

---
+++ src/b13/clsssss.java
@@ -0,0 +1,14 @@
+package b13;
+public class clsssss {
+public static void main(String[] args) {
+    for (int i = 1; i < 100; i++) {
+        if (i % 2 == 0) {
+            //блок кода 1
+        }
+        else{
+             System.out.println(i + " ");
+        }
+
+     }
+  }
+}
